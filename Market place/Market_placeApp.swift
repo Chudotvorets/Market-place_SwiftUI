52a3948472aa67d32cc9ea4f657f9beea894b8f8
Chudotvorets <simonovevgvlad@gmail.com>
2024-11-14 16:45:51 +0400

    Initial Commit

---
+++ Market place/Market_placeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Market_placeApp.swift
+//  Market place
+//
+//  Created by dev on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Market_placeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Market placeTests/Market_placeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Market_placeTests.swift
+//  Market placeTests
+//
+//  Created by dev on 14.11.2024.
+//
+
+import Testing
+@testable import Market_place
+
+struct Market_placeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
