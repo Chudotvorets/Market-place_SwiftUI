14a43aec69d33114d49b289ade4ec6031ad5849a
Chudotvorets <simonovevgvlad@gmail.com>
2024-11-14 17:00:55 +0400

    Создание UI интерфейса.

---
+++ ProductModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProductModel.swift
+//  Market place
+//
+//  Created by dev on 14.11.2024.
+//
+
+import Foundation
---
+++ ProductService.swift
@@ -0,0 +1,8 @@
+//
+//  ProductService.swift
+//  Market place
+//
+//  Created by dev on 14.11.2024.
+//
+
+import Foundation
